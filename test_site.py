f7af02f574e33b3e66a40f08dcb9d8a66da33e2c
Yuri Primyshev <p020885@yandex.ru>
2024-08-28 13:26:18 -0400

    2

--- test_site.py
+++ test_site.py
@@ -1,6 +1,7 @@
 from selenium import webdriver # для взаимодействия с браузером
 from selenium.webdriver.common.by import By # для поиска элементов на веб-странице
 import pytest
+import time # для ожидания
 
 @pytest.fixture
 def browser(): # обычно пишут не browser, а driver
@@ -15,4 +16,12 @@ def test_open_galaxy_s6(browser):
     galaxy_s6 = browser.find_element(By.XPATH, value='//a[text()="Samsung galaxy s6"]') # ищем элемент и сохраняем в переменную
     galaxy_s6.click()
     title = browser.find_element(By.CSS_SELECTOR, value='h2')
-    assert title.text=='Samsung galaxy s6'
+    assert title.text == 'Samsung galaxy s6'
+
+def test_two_monitors(browser):
+    browser.get('https://demoblaze.com/index.html')
+    monitor_link=browser.find_element(By.CSS_SELECTOR, value='''[onclick="byCat('monitor')"]''')
+    monitor_link.click()
+    time.sleep(3)
+    monitors = browser.find_elements(By.CSS_SELECTOR, value='.card')
+    assert len(monitors) == 2
